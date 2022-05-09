4286f5008d5adde201b246bf0ce0edc108eeb518
RostoRM <92208542+RostoRM@users.noreply.github.com>
2022-05-09 12:34:14 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+//Cash DOM
+const css = document.querySelector("h3");
+const input_Color1 = document.querySelector(".input1");
+const input_Color2 = document.querySelector(".input2");
+const body = document.getElementById("gradient");
+const btn = document.getElementById("btn");
+
+//rendomNumber generator
+const random = () => {
+  return Math.floor(Math.random() * 256);
+};
+//attach linear-gradient propertiesto backgrounds
+const showRendomColor = () => {
+  let RgbColorProperty = `linear-gradient(to right,
+  rgb(${random()},${random()},${random()}),
+  rgb(${random()},${random()},${random()}))`;
+
+  body.style.background = RgbColorProperty;
+  btn.style.background = RgbColorProperty;
+  css.textContent = RgbColorProperty + `;`;
+};
+//function for input colors
+const setGradient = () => {
+  let RgbColorProperty = `linear-gradient(to right, ${input_Color1.value}, ${input_Color2.value})`;
+  body.style.background = RgbColorProperty;
+  btn.style.background = RgbColorProperty;
+  css.textContent = RgbColorProperty + `;`;
+};
+//onload set default Gradients properties
+window.onload = setGradient();
+//execution main function
+const main = () => {
+  input_Color1.addEventListener("input", setGradient);
+  input_Color2.addEventListener("input", setGradient);
+  btn.addEventListener("click", showRendomColor);
+};
+
+main();
